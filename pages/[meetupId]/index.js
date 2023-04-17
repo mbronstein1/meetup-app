7d186c65fbb6fd6cecc40d14d9040198bb2ecf95
Matthew Bronstein <mbronstein1@gmail.com>
2023-04-17 13:50:29 -0500

    modularized meetup detail into separate component with style

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '@/components/meetups/MeetupDetail';
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image='https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2l0eXxlbnwwfHwwfHw%3D&w=1000&q=80'
+      title='First Meetup'
+      address='Some Street 5, Some City'
+      description='This is a first meetup'
+    />
+  );
+};
+
+export default MeetupDetails;
