b7654c42f48d7c603789851349c00614577c57b6
Matthew Bronstein <mbronstein1@gmail.com>
2023-04-17 13:07:42 -0500

    added homepage w/ dummy data

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'A First Meetup',
+    image: 'https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2l0eXxlbnwwfHwwfHw%3D&w=1000&q=80',
+    address: 'Some address 5, 12345 Some City',
+    description: 'This is a first meetup',
+  },
+  {
+    id: 'm2',
+    title: 'A Second Meetup',
+    image: 'https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2l0eXxlbnwwfHwwfHw%3D&w=1000&q=80',
+    address: 'Some address 5, 12345 Some City',
+    description: 'This is a second meetup',
+  },
+];
+
 const HomePage = () => {
-  return <h1>Home Page</h1>;
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 };
 
 export default HomePage;
