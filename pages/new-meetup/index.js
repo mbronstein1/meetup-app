39b1d79fbc41947e80390d5e0164056a1051765f
Matthew Bronstein <mbronstein1@gmail.com>
2023-04-17 13:13:44 -0500

    added new meetup form

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,12 @@
+import NewMeetupForm from '@/components/meetups/NewMeetupForm';
+import React from 'react';
+
+const NewMeetupPage = () => {
+  const addMeetupHandler = enteredMeetupData => {
+    console.log(enteredMeetupData);
+  };
+
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+};
+
+export default NewMeetupPage;
