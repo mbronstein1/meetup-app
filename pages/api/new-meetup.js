010e252c4548377bbbc71df6581bb9644f104fa9
Matthew Bronstein <mbronstein1@gmail.com>
2023-04-18 12:31:29 -0500

    added api folder and new-meetup post request to connect and post to DB

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,21 @@
+// route: /api/new-meetup
+import { MongoClient } from 'mongodb';
+
+const handler = async (req, res) => {
+  if (req.method === 'POST') {
+    const data = req.body;
+
+    const client = await MongoClient.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dxxsy0n.mongodb.net/meetups?retryWrites=true&w=majority`);
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+    const result = await meetupsCollection.insertOne(data);
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: 'Meetup inserted!' });
+  }
+};
+
+export default handler;
